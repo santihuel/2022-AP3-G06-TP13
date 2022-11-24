bd666f98b43a69e0c63a0e5f14900573a1414a00
Replit user <>
2022-11-24 15:51:12 -0300

    tp13

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //Constantes del juego
-const COLUMNAS = 10;
-const FILAS = 10;
-const CANTIDAD_MINAS = 10;
+const COLUMNAS = 15;
+const FILAS = 15;
+const CANTIDAD_MINAS = 30;
 
 //Variables con colores para los casilleros (NO se pudieron declarar como constantes ya que  la fn color sólo está definida para el setup y el draw)
 var COLOR_CASILLERO_CON_MINA;
@@ -16,49 +16,134 @@ var hizoClick = false;
 //Otras variables
 var casillerosSinDescubrir;
 
-
 function setup()
 {
+  casillerosSinDescubrir = FILAS*COLUMNAS;
+  
   createCanvas(500, 500);   //crea un lienzo o panel donde estará el juego. El primer parámetro es el ancho y el segundo el alto del lienzo.
   laMagiaDeLosProfes();
 
-  //Asigno colores que se utilizarán. La fn color solo está definida para el setup y el draw
+  //Asigno colores que se utilizarán. La fn color solo    está definida para el setup y el draw
   COLOR_CASILLERO_CON_MINA = color("#FF0000");
   COLOR_CASILLERO_SIN_MINA = color("#1CC932");
   COLOR_CASILLERO_MARCADO = color("#278EF2");
 
   // Modificar/completar
+  ponerMinasTablero();
 }
-
 
 function draw() {
   if (hizoClick == true)
   {
-    pintarCasillero(columnaPresionada, filaPresionada, COLOR_CASILLERO_SIN_MINA); //pinta el casillero clickeado. Modificar/completar
-
-
-    
+    if(mouseButton === LEFT)
+    {
+      if(tieneMinaCasillero(columnaPresionada, filaPresionada))
+      {
+        perder(); 
+      }
+      else
+      {
+        descubrirCasillero(columnaPresionada, filaPresionada);
+        if (casillerosSinDescubrir === CANTIDAD_MINAS)
+          {
+            ganoElJuego();
+          }
+      }
+    }
+    else if (mouseButton === RIGHT) 
+    {
+     pintarCasillero(columnaPresionada, filaPresionada, COLOR_CASILLERO_MARCADO); 
+    }
+      
     hizoClick = false;  //Indico que ya "procesé" el click del usuario. NO modificar
   }
 }
 
-
 function ganoElJuego()
 {
-  return false;   //Esto hace que NUNCA gane el juego. Modificar/completar
+  return ganar();   //Esto hace que NUNCA gane el juego. Modificar/completar
 }
 
 function ponerMinasTablero()
 {
-  // Modificar/completar
+  for (let contador = 0; contador < CANTIDAD_MINAS; contador++)
+    {
+      var col = floor(random(0, COLUMNAS));
+      var fil = floor(random(0, FILAS));
+      
+      if(tieneMinaCasillero(col, fil))
+      {
+        contador--;
+        continue;
+      }
+      else
+      {
+        ponerMinaCasillero(col, fil);
+      }
+    }
 }
 
-function mostrarMinas()
+function mostrarMinas(contCol, contFil)
 {
-  // Modificar/completar
+  for (var contFila = 0; contFila < FILAS; contFila++)
+  {
+    recorrerFila(contFila);
+  }
 }
 
 function contarMinasAlrededor(columna, fila)
 {
-  return 9;   //Esto hace que SIEMPRE cuente 9 minas alrededor. Modificar/completar
+  var contador = 0;
+  if (tieneMinaCasillero (columna -1, fila -1))
+  {
+    contador++;  
+  }
+
+  if (tieneMinaCasillero (columna, fila -1))
+  {
+    contador++;  
+  }
+
+  if (tieneMinaCasillero (columna +1, fila -1))
+  {
+    contador++;  
+  }
+
+  if (tieneMinaCasillero (columna -1, fila))
+  {
+    contador++;  
+  }
+
+  if (tieneMinaCasillero (columna +1, fila))
+  {
+    contador++;  
+  }
+
+  if (tieneMinaCasillero (columna -1, fila +1))
+  {
+    contador++;  
+  }
+
+  if (tieneMinaCasillero (columna, fila +1))
+  {
+    contador++;  
+  }
+
+if (tieneMinaCasillero (columna +1, fila +1))
+  {
+    contador++;  
+  }
+  
+  return contador;
 }
+
+function recorrerFila(fila)
+{
+  for (var contCol = 0; contCol < COLUMNAS; contCol++)
+      {
+        if (tieneMinaCasillero(contCol, fila))
+        {
+          pintarCasillero(contCol, fila, COLOR_CASILLERO_CON_MINA);
+        }
+      }    
+} 
--- respuestas.md
+++ respuestas.md
@@ -1,5 +1,11 @@
 ## Respuesta punto 6
+  El valor de la variable deberia ser de 25.
+
+## Respuesta punto 7
+  En el setup().
 
 ## Respuesta punto 8
+  Deberia valer 7.
 
-## Respuesta punto ...
+## Respuesta punto 10
+  En el Draw() luego de que se confirme que la cantidad de casilleros sin descubrir es igual a la CANTIDAD_MINAS.
